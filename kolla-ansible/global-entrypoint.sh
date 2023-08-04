ae2ebff3a962023d03e47ecfc2265edfa968363c
Can Özyurt <acozyurt@gmail.com>
2023-08-04 14:13:26 +0000

    put inventories in a folder for k-a

--- kolla-ansible/global-entrypoint.sh
+++ kolla-ansible/global-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [ ! -f /etc/kolla/passwords.yml ]; then
 	cp /usr/local/share/kolla-ansible/etc_examples/kolla/passwords.yml /etc/kolla/passwords.yml
@@ -8,5 +9,12 @@ fi
 if [ ! -f /etc/kolla/globals.yml ]; then
 	cp /usr/local/share/kolla-ansible/etc_examples/kolla/globals.yml /etc/kolla/globals.yml
 fi
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+cp -R /tmp/.ssh /root/.ssh
+chmod 700 /root/.ssh
+chmod 644 /root/.ssh/id_rsa.pub
+chmod 600 /root/.ssh/id_rsa
 
 exec "$@"
