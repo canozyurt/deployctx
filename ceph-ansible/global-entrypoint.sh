4aad52f075695f66bad8ef3ceceafb1a0ab3bc1c
Can Özyurt <acozyurt@gmail.com>
2023-09-22 12:50:52 +0000

    parameter option through extra var

--- ceph-ansible/global-entrypoint.sh
+++ ceph-ansible/global-entrypoint.sh
@@ -3,4 +3,11 @@
 mv site-container.yml.sample site-container.yml
 mv site.yml.sample site.yml
 
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+cp -R /tmp/.ssh /root/.ssh
+chmod 700 /root/.ssh
+chmod 644 /root/.ssh/id_rsa.pub
+chmod 600 /root/.ssh/id_rsa
+
 exec "$@"
